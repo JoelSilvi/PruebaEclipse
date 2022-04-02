242285a72ce68c4e8d2adeb8241a95af2bf7bf28
JoelSilvi <analistasilvi@gmail.com>
2022-04-02 00:16:59 -0300

    primer

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		int num = 122354;
+		System.out.println("Hola el numero es: " + num);
+		
+	}
+}
---
+++ PrincipalClase.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class PrincipalClase {
+
+	public static void main(String[] args) {
+		int num = 154545;
+		System.out.println("Hola el numero es: " + num);
+		
+	}
+}
